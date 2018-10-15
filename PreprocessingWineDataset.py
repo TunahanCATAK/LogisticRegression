10814d92ce128eba02f7b39ce6482e3ca07b475d
cataktunahan <cataktunahan@gmail.com>
2018-10-15 22:00:08 +0300

    Initial Commit for WineData set preprocessing.

---
+++ PreprocessingWineDataset.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD820/OD315 of diluted wines',
+                   'Proline']
+
+print('Class labels', np.unique(df_wine['Class label']))
+print(df_wine.head())
